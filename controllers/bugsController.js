ccdcfac45a1c5f820bde56ebff9260a8aeae69fd
lilianavillar <lilianavillar@hotmail.es>
2017-04-24 06:10:41 +0200

    Fix bugs

--- controllers/bugsController.js
+++ controllers/bugsController.js
@@ -5,7 +5,7 @@ var bugs = mongoose.model('bugs');
 //Método encargado de devolver todos los BUGS de un SPRINT
 exports.findAllBugs = function(req, res) {
     console.log('Get/bugs' + req.params.id);
-    sprints.find({'_id': req.params.id}, 'bugs', function(err, bugs) {
+    sprints.find({"nombre": req.params.id}, 'bugs', function(err, bugs) {
         if(err){
             res.send(500, err.message);
         }
